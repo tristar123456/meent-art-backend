5b1dded9fef9cfbc26525b092320bfda5def8f62
Tristan Wolf <tristan.wolf@rub.de>
2021-04-02 14:41:29 +0200

    :construction: wip

--- api/settings.py
+++ api/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ku3oh&j0(st6lxv^$tmx@qyg@14+c3tr1y*i6dyhpb=akrwea='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 CORS_ORIGIN_WHITELIST = (
