33e73eece32f7ea169a9cc4977d13e56d518685b
Tristan Wolf <tristan.wolf@rub.de>
2021-04-05 21:14:36 +0200

    :art: Improvements

--- userManagement/models.py
+++ userManagement/models.py
@@ -11,6 +11,7 @@ def return_date_time():
     return now + timedelta(days=1)
 
 class Token(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, null=False)
     user = models.ForeignKey(User, default=User, on_delete=models.CASCADE, null=True)
     api_token = models.UUIDField(default=uuid.uuid4, null=False)
     date = models.DateTimeField(default=return_date_time, null=True)
