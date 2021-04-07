104827d3f66844159ed133cc684b7609b23cc700
Tristan Wolf <tristan.wolf@rub.de>
2021-04-07 16:59:29 +0200

    :art: Improvements

--- api/settings.py
+++ api/settings.py
@@ -115,19 +115,7 @@ WSGI_APPLICATION = 'api.wsgi.application'
 #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgresql-metric-08898',
-        'USER': 'u3123',
-        'PASSWORD': 'MeineKunstIstEinzigartig1!',
-        'HOST': 'localhost',
-        'PORT': '',
-    }
-}
-
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+DATABASES = {'default': dj_database_url.config(conn_max_age=600, ssl_require=True)}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
